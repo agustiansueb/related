7848198e6d04b8d9b8e42db350f5bba8fc2199cf
agustiansueb <18346074+agustiansueb@users.noreply.github.com>
2021-01-01 14:44:53 +0700

    Update click.js

--- click.js
+++ click.js
@@ -1,7 +1,19 @@
 	
-					removeRelatedDuplicates();  
-					printRelatedLabels();  
-				
-//<![CDATA[  
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 4=g f();2 9=0;2 6=g f();d z(m){c(2 i=0;i<m.u.5.3;i++){2 5=m.u.5[i];4[9]=5.v.$t;c(2 k=0;k<5.h.3;k++){b(5.h[k].A==\'y\'){6[9]=5.h[k].x;9++;C}}}}d B(){2 7=g f(0);2 8=g f(0);c(2 i=0;i<6.3;i++){b(!q(7,6[i])){7.3+=1;7[7.3-1]=6[i];8.3+=1;8[8.3-1]=4[i]}}4=8;6=7}d q(a,e){c(2 j=0;j<a.3;j++)b(a[j]==e)p M;p L}d D(){2 r=s.K((4.3-1)*s.I());2 i=0;n.l(\'<o>\');E(i<4.3&&i<F){n.l(\'<w><a x="\'+6[r]+\'" J ="G" v="\'+4[r]+\'">\'+4[r]+\'</a></w>\');b(r<4.3-1){r++}H{r=0}i++}n.l(\'</o>\')}',49,49,'||var|length|relatedTitles|entry|relatedUrls|tmp|tmp2|relatedTitlesNum||if|for|function||Array|new|link||||write|json|document|ul|return|contains||Math||feed|title|li|href|alternate|related_results_labels|rel|removeRelatedDuplicates|break|printRelatedLabels|while|20|_blank|else|random|target|floor|false|true'.split('|'),0,{}))
-//]]>  
+			var labelArray = [<b:if cond='data:post.labels'><b:loop values='data:post.labels' var='label'>&quot;<data:label.name/>&quot;<b:if cond='data:label.isLast != &quot;true&quot;'>, </b:if></b:loop></b:if>];
+			var relatedPostConfig = {
+			homePage: &quot;<data:blog.homepageUrl/>&quot;,
+			widgetTitle: &quot;&lt;div class=&#39;label-line-c&#39;&gt;&lt;h4&gt;Related Posts&lt;/h4&gt;&lt;/div&gt;&quot;,
+			numPosts: 4,
+			summaryLength: 370,
+			titleLength: &quot;auto&quot;,
+			thumbnailSize: 250,
+			noImage: &quot;//1.bp.blogspot.com/-lbF_-uMhOUk/XsPnhv8SPxI/AAAAAAAAJRQ/O7KqrfJdN8wqJyyCbarikKFLO29lHMg1wCLcBGAsYHQ/s1600/Screenshot_2020-05-19-21-02-44-1.png&quot;,
+			containerId: &quot;related-post-<data:post.id/>&quot;,
+			newTabLink: false,
+			moreText: &quot;Read More&quot;,
+			widgetStyle: 3,
+			callBack: function() {}
+			};
+	
+
+					
